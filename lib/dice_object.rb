39b9e59150deaeebd954ee6a39acb098f0f4c8e4
Oliver <ofcharrison@gmail.com>
2019-02-08 12:23:05 +0000

    Can roll multiple di/die/dice?

--- lib/dice_object.rb
+++ lib/dice_object.rb
@@ -1,2 +1,10 @@
 class Dice
+  def roll
+  rand(1..6)
+  end
+
+  def many_dice(dice)
+  dice.times do puts rand(1..6)
+             end
+  end
 end
--- spec/dice_object_spec.rb
+++ spec/dice_object_spec.rb
@@ -2,5 +2,16 @@ require 'dice_object'
 
 describe Dice do
   it 'is should give a number between 1 & 6' do
-end
+    dice = Dice.new
+    dice.roll
+    x = (1..6).to_a
+    expect(x).to include(dice.roll)
+  end
+
+puts "----------"
+
+  it 'should roll many dice' do
+    dice = Dice.new
+    expect(dice.many_dice(3)).to eq(3)
+  end
 end
